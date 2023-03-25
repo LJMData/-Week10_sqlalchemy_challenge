99c0681fb59d7a26ee1a43d74a822d5f87014cce
LJMData <luciemullany@gmail.com>
2023-03-25 22:51:39 +0000

    Final_files

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# -Week10_sqlalchemy_challenge
+# Hawaii Weather Analysis
+
+Part 1: Analysis 
+
+The purpose of this code is to analyze climate data from Hawaii using SQLAlchemy and pandas in Python. The code connects to an SQLite database with two tables: measurement and station. The measurement table contains information about precipitation, temperature, and other weather data, while the station table contains information about weather stations. The code analyzes and visualizes the data to provide insights into Hawaii's climate.
+
+Exploratory Precipitation Analysis
+In this section, the code retrieves the precipitation data from the measurement table and saves it as a pandas DataFrame. The code also calculates the summary statistics for the precipitation data and plots it over time.
+
+Exploratory Station Analysis
+In this section, the code retrieves the station data from the station table and calculates the number of stations in the dataset. The code also finds the most active station and calculates the lowest, highest, and average temperature for that station. Finally, the code plots a histogram of the temperature data for the most active station
+
+Part 2: Flask App
+
+This code is a Flask app that retrieves weather data from an database for Hawaii climate. The app has the following functionality:
+
+/api/v1.0/precipitation: Returns a JSON representation of precipitation observations recorded in Hawaii from 2016-08-23 to the last available date in the dataset. The results are grouped by date.
+
+/api/v1.0/stations: Returns a JSON list of stations from which weather data were recorded.
+
+/api/v1.0/tobs: Returns a JSON representation of temperature observations recorded in Hawaii from 2016-08-23 to the last available date in the dataset for the most active station.
+
+/api/v1.0/start_date: Returns a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a given date range. The start_date should be in the format YYYY-mm-dd.
+
+/api/v1.0/start_date/end_date: Returns a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a given date range. Both the start_date and end_date should be in the format YYYY-mm-dd.
+
+An example of the code used is 
+
+```ruby 
+@app.route("/api/v1.0/precipitation")
+def precip():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #create a dictionary of the precipitation
+    start_date = '2016-08-23'
+    results = [measurement.date, measurement.prcp]
+    precip_results = session.query(*results).\
+            filter(measurement.date >= start_date).\
+            group_by(measurement.date).\
+            order_by(measurement.date).all()
+
+    session.close()
+
+    precip_results_final =[]
+
+    for date, prcp in precip_results:
+        precip_dict = {}
+        precip_dict["date"] = date
+        precip_dict["precipitation"] = prcp
+        precip_results_final.append(precip_dict)
+
+    return jsonify(precip_results_final)
+```
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,173 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
-
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Hawaii Weather Data:<br/><br>"
+        f"<a href=\"/api/v1.0/precipitation\">/api/v1.0/precipitation<a><br/>"
+        f"<a href=\"/api/v1.0/stations\">/api/v1.0/stations<a><br/>"
+        f"<a href=\"/api/v1.0/tobs\">/api/v1.0/tobs<a><br/>"
+        f"/api/v1.0/start_date<br/>"
+        f"/api/v1.0/start/end<br/>"
+        f"Note: Please ensure you enter both dates using format: YYYY-mm-dd/YYYY-mm-dd"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precip():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #create a dictionary of the precipitation
+    start_date = '2016-08-23'
+    results = [measurement.date, measurement.prcp]
+    precip_results = session.query(*results).\
+            filter(measurement.date >= start_date).\
+            group_by(measurement.date).\
+            order_by(measurement.date).all()
+
+    session.close()
+
+    precip_results_final =[]
+
+    for date, prcp in precip_results:
+        precip_dict = {}
+        precip_dict["date"] = date
+        precip_dict["precipitation"] = prcp
+        precip_results_final.append(precip_dict)
+
+    return jsonify(precip_results_final)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #Create a list of stations 
+    station_query_results = session.query(Station.station,Station.id).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list
+    stations_all = []
+    for station, id in station_query_results:
+        stations_values_dict = {}
+        stations_values_dict['station'] = station
+        stations_values_dict['id'] = id
+        stations_all.append(stations_values_dict)
+
+    return jsonify (stations_all)
+
+@app.route("/api/v1.0/tobs")
+def temps():
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query the data
+    results = session.query(measurement.date, measurement.tobs, measurement.prcp).\
+        filter(measurement.date >= '2016-08-23').\
+        filter(measurement.station=='USC00519281').\
+        order_by(measurement.date).all()
+    
+    session.close()
+
+    # Create a dictionary using the query results 
+
+    all_temps = []
+    for prcp, date, tobs in results:
+        temps_dict = {}
+        temps_dict["precip"] = prcp
+        temps_dict["date"] = date
+        temps_dict["temp"] = tobs
+        
+        all_temps.append(temps_dict)
+
+    return jsonify(all_temps)
+
+@app.route("/api/v1.0/<start_date>")
+def date_1(start_date, end_date='2017-08-23'):
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #Calculate minimum, average and maximum temperatures for the date range.
+    query_result = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= start_date).filter(measurement.date <= end_date).all()
+    
+    session.close()
+
+    #Create a dictionary
+    date_stats = []
+
+    for min, avg, max in query_result:
+        date_dict = {}
+        date_dict["Min"] = min
+        date_dict["Average"] = avg
+        date_dict["Max"] = max
+        date_stats.append(date_dict)
+
+    return jsonify(date_stats)
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def date_2(start_date, end_date):
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #Calculate minimum, average and maximum temperatures for the date range.
+    query_result2 = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= start_date).filter(measurement.date <= end_date).all()
+    
+    session.close()
+
+    #Create a dictionary
+    date2_stats = []
+
+    for min, avg, max in query_result2:
+        date2_dict = {}
+        date2_dict["Min"] = min
+        date2_dict["Average"] = avg
+        date2_dict["Max"] = max
+        date2_stats.append(date2_dict)
+
+    return jsonify(date2_stats)
